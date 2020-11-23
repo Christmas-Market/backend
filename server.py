9297f84e28a7da1e47964cddbd7050e788238cab
Sébastien Combéfis <seb478@gmail.com>
2020-11-23 22:19:59 +0100

    Replace f-strings with formatted strings

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, status
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 
 import json
@@ -15,7 +15,7 @@ def home():
 @app.route('/placeorder', methods=['POST'])
 def placeorder():
     print('>>> Placing a new order...')
-    url = f"https://www.google.com/recaptcha/api/siteverify?secret={os.environ['RECAPTCHA_SECRET_KEY']}&response={request.json['token']}"
+    url = 'https://www.google.com/recaptcha/api/siteverify?secret={}&response={}'.format(os.environ['RECAPTCHA_SECRET_KEY'], request.json['token'])
     resp = requests.post(url)
     if resp.status_code == 200:
         result = resp.json()
@@ -26,7 +26,7 @@ def placeorder():
             cart = json.loads(request.json['cart'].encode('utf-8'))
             print(cart)
             return jsonify({'ok': True})
-    return jsonify({'ok': False}), status.HTTP_400_BAD_REQUEST
+    return abort(400)
 
 if __name__ == '__main__':
     app.run()
